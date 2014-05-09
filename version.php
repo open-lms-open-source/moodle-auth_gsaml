f1f9f8ecf2f0a1c63f625b2da4e141bb811732eb
mrcroy <chris@moodlerooms.com>
2014-05-09 09:43:17 -0400

    INT-5972: Adding versions to various third party plugins

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * eFolio Authentication Plugin
+ *
+ * @author Mark Nielsen
+ * @version $Id$
+ * @package auth_gsaml
+ **/
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2013110500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013110500;        // Requires this Moodle version
+$plugin->component = 'auth_gsaml';      // Full name of the plugin (used for diagnostics)
