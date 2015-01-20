e545c847a1059852f3967e7506e1d57ee7c808e4
Mark Nielsen <mark@moodlerooms.com>
2015-01-20 13:36:19 -0800

    INT-7245: Prepare auth_gsaml for open source

--- version.php
+++ version.php
@@ -1,5 +1,25 @@
 <?php
 /**
+ * Copyright (C) 2009  Moodlerooms Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * @copyright  Copyright (c) 2009 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license    http://opensource.org/licenses/gpl-3.0.html     GNU Public License
+ */
+
+/**
  * Google SAML Authentication Plugin
  *
  * @author Chris Stones
@@ -8,6 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013110500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013110500;        // Requires this Moodle version
-$plugin->component = 'auth_gsaml';      // Full name of the plugin (used for diagnostics)
+$plugin->version      = 2013110500;
+$plugin->requires     = 2014051203;
+$plugin->component    = 'auth_gsaml';
+$plugin->release      = '2.7.0 (Build: 20141229)';
+$plugin->dependencies = array('local_mr' => 2010090201);
