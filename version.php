ff5481e80e483d1fafa61005dd37e67bd39831e5
Mark Nielsen <mark@moodlerooms.com>
2017-02-14 08:55:17 -0800

    INT-11133: bump requires and release for Moodle 3.2.1

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version      = 2016121300;
-$plugin->requires     = 2016052300;
+$plugin->requires     = 2016120500;
 $plugin->component    = 'auth_gsaml';
-$plugin->release      = '3.1.3';
+$plugin->release      = '3.2.1';
 $plugin->dependencies = array('local_mr' => 2016121300);
