8ab6619bb9a277fbae5ce159e05f8457a053949c
Mark Nielsen <mark@moodlerooms.com>
2017-12-08 15:42:58 -0800

    INT-12724: Bump version and release

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version      = 2017063000;
+$plugin->version      = 2017120800;
 $plugin->requires     = 2017051500;
 $plugin->component    = 'auth_gsaml';
-$plugin->release      = '3.3.1';
-$plugin->dependencies = array('local_mr' => 2017063000);
+$plugin->release      = '3.3.3';
+$plugin->dependencies = array('local_mr' => 2017120800);
