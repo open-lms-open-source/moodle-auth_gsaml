2072f174f1e3d82728c9f21a239d3ba5753af45a
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2023-03-22 21:00:36 +0000

    INT-18815: Importing fi lang

--- lang/fi/auth_gsaml.php
+++ lang/fi/auth_gsaml.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2022 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -51,14 +51,16 @@ $string['uploadthekey'] = 'Lataa avain';
 $string['gadgetinfostr'] = 'Lisää Moodle-pienoisohjelma Google-aloitussivullesi seuraavan verkko-osoitteen avulla<br/><b>{$a->wwwroot}/auth/gsaml/moodlegadget.php</b>';
 $string['lnktogoogsettings'] = 'Linkki Google-asetuksiin';
 $string['nodomainyet'] = 'Toimialuetta ei ole määritetty';
-$string['gsamlsetuptableinfo'] = '<ol><li>Määritä <b>Toimialuenimi</b>-Google-palvelun toimialuenimen kohtaan ja napsauta <b>Tallenna muutokset</b>.<br/><br/></li>
-<li>Avaa Google Apps -hallintakonsoli uuteen ikkunaan järjestelmänvalvojana (<a href="https://www.google.com/a/{$a->domainname}">{$a->googsettings}</a>).<br/><br/></li>
-<li>Napsauta<b>Lisätyökalut</b>-välilehteä.<br/><br/></li>
-<li>Napsauta sitten <b>Set up single sign-on (SSO)</b> (Määritä kertakirjautuminen) -linkkiä kohdassa Authentication (Todennus).<br/><br/></li>  <li>Merkitse ensin <b>Enable Single Sign-on</b> (Ota kertakirjautuminen käyttöön) -valintaruutu.<br/><br/></li>
-<li>Lisää tämä URL-osoite <b>Sign-in page URL</b> (Kirjautumissivun osoite) -tekstikenttään.<br/><b>{$a->wwwroot}/login/index.php</b><br/><br/></li>  <li>Lisää tämä URL-osoite <b>Sign-out page URL</b> (Uloskirjautumissivun osoite) -tekstikenttään.<br/><b>{$a->wwwroot}/login/logout.php</b><br/><br/></li>
-<li>Lisää tämä URL-osoite <b>Change password URL</b> (Salasanan vaihdon osoite) -tekstikenttään.<br/><b>{$a->wwwroot}/login/change_password.php</b><br/><br/></li>
-<li>Luo ja lataa <b>tarkistusvarmenne Googleen (julkisen avaimen sisältämä X.509-varmenne).</b><br/><br/></li>
-<li>Lataa yksityinen avain ja varmenne myös Moodleen ja napsauta sitten <b>Tallenna muutokset</b></b>.<br/></li></ol>';
+$string['gsamlsetuptableinfo'] = '<ol><li>Määritä <b>Toimialuenimi</b>-kohtaan Google-palvelun toimialuenimi ja valitse <b>Tallenna muutokset</b>.<br/><br/></li>
+<li>Avaa uudessa ikkunassa Google Apps Control Panel -sivu ylläpitäjänä (<a href="https://www.google.com/a/{$a->domainname}">{$a->googsettings}</a>).<br/><br/></li>
+<li>Napsauta <b>Advanced tools</b> -välilehteä.<br/><br/></li>
+<li>Napsauta Authentication-kohdan vieressä olevaa <b>Set up single sign-on (SSO)</b> -linkkiä.<br/><br/></li>
+<li>Valitse ensin <b>Enable Single Sign-on</b> -ruutu.<br/><br/></li>
+<li>Syötä nyt tämä URL-osoite <b>Sign-in page URL</b> -tekstikenttään.<br/><b>{$a->wwwroot}/login/index.php</b><br/><br/></li>
+<li>Syötä tämä URL-osoite <b>Sign-out page URL</b> -tekstikenttään.<br/><b>{$a->wwwroot}/login/logout.php</b><br/><br/></li>
+<li>Syötä tämä URL-osoite <b>Change password URL</b> -tekstikenttään.<br/><b>{$a->wwwroot}/login/change_password.php</b><br/><br/></li>
+<li>Luo ja lataa <b>Tarkistusvarmenne Googlea varten (X.509-varmenne, joka sisältää julkisen avaimen)</b>.<br/><br/></li>
+<li>Lataa yksityinen avain ja varmenne myös Moodleen ja valitse <b>Tallenna muutokset</b>.</b><br/></li></ol>';
 $string['tomoodle'] = 'Moodleen';
 $string['errusernotloggedin'] = 'Käyttäjää ei voitu kirjata sisään';
 $string['pwdnotvalid'] = 'Salasana ei kelpaa';
@@ -81,7 +83,7 @@ $string['gmailblocknotinstalled'] = 'Gmail-lohkoa ei ole asennettu';
 $string['gmailblockinstalled'] = 'Gmail-lohko on asennettu\\n';
 $string['gdataapitestresults'] = 'GData API -tekstitulokset';
 $string['trytoinitgdataconnection'] = 'Yritetään muodostaa yhteyttä GData-lohkon ja Googlen välille<br/>';
-$string['gsamlsuccess'] = 'Tila';
+$string['gsamlsuccess'] = 'Onnistui';
 $string['gmailtestresults'] = 'Gmail-testitulokset';
 $string['gmailtestwillnotrun'] = 'Gmail-testiä ei voi suorittaa, jos Moodle ole DEBUG_DEVELOPER-tilassa';
 $string['obtainemailfeed'] = 'Haetaan sähköpostisyötettä käyttäjänimelle:';
@@ -103,13 +105,22 @@ $string['cmid'] = 'cmid';
 $string['action'] = 'Toimenpide';
 $string['url'] = 'Web-osoite';
 $string['info'] = 'Informaatio';
+$string['contextinstanceid'] = 'Ilmentymän tunnus';
+$string['courseid'] = 'Kurssitunnus';
+$string['eventname'] = 'Tapahtuman nimi';
+$string['otherlog'] = 'Muu';
+$string['useraccesedgservice'] = 'Käyttäjä on käyttänyt ensin gserviceä';
+$string['userneedsredirect'] = 'Käyttäjällä on saml-pyyntö, mutta vaaditaan kirjautuminen/uudelleenohjaus';
+$string['userislogged'] = 'Käyttäjä on kirjautunut ja hänellä on saml-pyyntö';
+$string['login'] = 'Kirjaudu';
 $string['config_gsaml'] = 'Google SAML:n määrittäminen';
-$string['config_gsaml_help'] = '<h2>Määritysohje</h2>
+$string['config_gsaml_help'] = '<h2>Suorita määritys</h2>
 
-<p>Google-palvelun täydellinen integrointi Moodleen edellyttää muutamien määritystoimien tekemistä. Tämä ohjetiedosto opastaa
-Google-todennuslisäosan määrittämisessä sekä GData-lohkon  ja Gmail-lohkon valmistelemisessa.</p>
+<p>Googlen ja Moodlen täydellisen integroinnin käyttöönotto vaatii joitakin määrityksiä. Tämä ohjetiedosto
+opastaa prosessissa, jossa määritetään Google-todennuksen lisäosa sekä GData-lohko ja
+Gmail-lohko.</p>
 
-<h4>Sisällys:</h4>
+<h4>Sisällysluettelo:</h4>
 <ul>
 <li><a href="#pre">Edellytykset</a></li>
 <li><a href="#status">Vaiheet</a></li>
@@ -117,7 +128,7 @@ Google-todennuslisäosan määrittämisessä sekä GData-lohkon  ja Gmail-lohkon
 <li><a href="#consider">Huomioitavaa</a></li>
 </ul>
 
-<h4><a name="status" href="#status">Google Apps -tila</a></h4>
+<h4><a name="status" href="#status">Google Appsin tila</a></h4>
 <div class="indent">
 
 
@@ -130,149 +141,151 @@ Google-todennuslisäosan määrittämisessä sekä GData-lohkon  ja Gmail-lohkon
 <li>blocks/gmail</li>
 <li>blocks/gdata</li>
 <li>blocks/gaccess</li>
-<li>blocks/mgadget (valinnainen beetaversio)</li>
+<li>blocks/mgadget (beeta-versiossa valinnainen)</li>
 </ul>
 </ol>
 </p>
 
 <h4><a name="pre" href="#steps">Vaiheet</a></h4>
 <ol>
-<li>Kirjaudu Moodleen järjestelmänvalvojana.</li>
-<li>Päivitä lohkotaulut valitsemalla <b>Ilmoitukset</b>.</li>
-<li>Ota käyttöön <b>Google-todennus</b> hallintasivulta Lisäosien hallinta.</li>
-<li>Valitse käyttäjien todennus sivulla <b>Google-todennus</b>. Tältä sivulta löydät tarvittavat ohjeet.</li>
-<li>Lisää Google-kumppanisivun toimialue.</li>
-<li>Lataa varmenne (lisätietoja sen luomisesta on <a
-href="http://code.google.com/apis/apps/articles/sso-keygen.html">Googlen avainten luontiin liittyvässä ohjeessa</a>).</li>
-<li>Lataa yksityinen avain (lisätietoja sen luomisesta on <a
-href="http://code.google.com/apis/apps/articles/sso-keygen.html">Googlen avainten luontiin liittyvässä ohjeessa</a>).</li>
-<li>Napsauta Tallenna.</li>
-<li>Lisää asianmukaiset URL-osoitteet Googlen SSO-sivulle ohjeiden mukaisesti.</li>
-<li>Ota SSO-toiminto käyttöön Google-sivustossa napsauttamalla merkki valintaruutuun.</li>
-<li>Ota API-valmistelu käyttöön Google-sivustossa, tai muuten käyttäjiä ei päivitetä.</li>
-<li>Varmista, että API-valmistelu on otettu käyttöön Google-sivustossa.</li>
-<li>Saatat joutua pyytämään lisää käyttäjätilejä Googlen puolella.</li>
-<li>Palaa Moodlen pääsivulle.</li>
-<li>Napsauta Muokkaa.</li>
-<li>Lisää GAccess-, GMail- ja Gdata-lohkot sivulle.</li>
-<li>Napsauta <b>Google Apps</b> -lohkossa <b>Asetukset</b>. Lisää määritystiedot.</li>
-<li>Napsauta Tila-linkkiä ja varmista, että Google Apps -lohko on määritetty oikein.</li>
-<li>Avaa Sivuston hallinta &gt; Tietoturva &gt; Sivuston käyttöehdot</li>
-<li>Tarkista salasanakäytäntö.</li>
-<li>Määritä salasanan pituudeksi vähintään 6 merkkiä (Googlen salasanakäytännön vaatimus).</li>
-<li>Lisää synkronoitavat käyttäjät GData-lohkoon. Voit tehdä tämän napsauttamalla Lisää synkronoitavat käyttäjät -linkkiä <b>Google Apps -lohkossa</b>.
-Voit tarkistaa tulokset asettamalla          GData-lohkon Cron-päivitysväliksi 1 min.</li>
-<li>Voit ladata valitut Moodle-käyttäjät Googleen suorittamalla Cron-tehtävän manuaalisesti kohdassa admin/cron.php. Synkronoinnin tulosten pitäisi näkyä näytössä.
-<b>Huomaa, että Moodle-käyttäjien synkronointi Googleen voi kestää <em>HYVIN</em> pitkään tässä beetaversiossa.</b></li>
+<li>Kirjaudu Moodleen ylläpitäjänä.</li>
+<li>Päivitä lohkotaulukot valitsemalla <b>Ilmoitukset</b>.</li>
+<li>Ota <b>Google-todennus</b>-lisäosa käyttöön Hallinnoi lisäosia -sivulta.</li>
+<li>Valitse käyttäjien todennus valitsemalla <b>Google-todennus</b> ja noudata sivulla olevia ohjeita.</li>
+<li>Siirry Googlen kumppanisivujen toimialueelle.</li>
+<li>Lataa varmenne (lisätietoja sen luonnista on kohdassa <a
+href="http://code.google.com/apis/apps/articles/sso-keygen.html"&gt;Google Documentation Regarding Key Generation</a>).</li>
+<li>Lataa yksityinen avain (lisätietoja sen luonnista on kohdassa <a
+href="http://code.google.com/apis/apps/articles/sso-keygen.html"&gt;Google Documentation Regarding Key Generation</a>).</li>
+<li>Valitse Tallenna.</li>
+<li>Noudata ohjeita ja lisää oikeat URL-osoitteet Googlen SSO-sivulle.</li>
+<li>Ota SSO käyttöön valitsemalla Google-sivustossa oleva valintaruutu.</li>
+<li>Varmista, että otat Google-sivustossa käyttöön valmistelun ohjelmointirajapinnan, tai käyttäjiä ei päivitetä.</li>
+<li>Varmista Google-sivustossa, että API Provisioning on valittu.</li>
+<li>Googlen puolelta voi olla tarpeen pyytää lisää käyttäjätilejä.</li>
+<li>Palaa Moodlen etusivulle.</li>
+<li>Valitse Muokkaa.</li>
+<li>Lisää GAccess-, Gmail- ja GData-lohkot sivulle.</li>
+<li>Valitse <b>Google Apps</b> -lohkossa <b>Asetukset</b>. Täytä määritystiedot.</li>
+<li>Vahvista Tila-linkkiä napsauttamalla, että Google Apps -lohko on määritetty oikein.</li>
+<li>Valitse Sivuston hallinta &gt; Tietoturva &gt; Sivuston käyttöehdot.</li>
+<li>Valitse Salasanakäytäntö.</li>
+<li>Määritä salasanan pituudeksi vähintään 6 (vaaditaan Googlen salasanakäytäntöä varten).</li>
+<li>GData-lohkoon on lisättävä käyttäjiä synkronointia varten. Tee tämä napsauttamalla Lisää synkronoitavia käyttäjiä -linkkiä <b>Google Apps
+-lohkossa</b>. Tuloksen näkee helpommin, kun GData-lohkon cron-asetukseksi määritetään 1 minuutti.</li>
+<li>Kun lataat Moodle-käyttäjiä Googleen, voit suorittaa cronin manuaalisesti siirtymällä tiedostoon admin/cron.php. Synkronoinnin tulosten pitäisi näkyä lukemassa.
+<b>Huomaa, että beeta-versiossa Moodle-käyttäjien synkronointi Googleen kanssa voi kestää <em>KAUAN</em>.</b></li>
 </ol>
 
 <h4><a name="pre" href="#expect">Odotetut tulokset</a></h4>
 <ul class="alternate" type="square">
-<li>Google-palvelujen linkkien napsauttaminen tai palvelujen avaaminen Google-kumppanin sivun
-kautta todentaa käyttäjän Moodleen.</li>
-<li>Käyttäjä kirjataan sekä Moodleen että Google-kumppanipalveluihin.</li>
-<li>Katso lisätietoja määrityksistä GSAML-asetusten<b>diagnostiikkasivulta</b>.</li>
+<li>Google-palvelujen linkkien napsauttaminen tai Googlen kumppanisivujen palveluissa käyminen
+suorittaa todennuksen Moodleen.</li>
+<li>Käyttäjien on kirjauduttava sekä Moodleen että Google-kumppanipalveluihin.</li>
+<li>Katso GSAML-asetusten <b>Diagnostiikkasivulta</b> lisätietoja määrityksestä.</li>
 </ul>
 
 
-<h4><a name="pre" href="#consider">Huomioitavaa (BEETA-versiosta)</a></h4>
+<h4><a name="pre" href="#consider">Huomioitavaa (BEETA-versiossa)</a></h4>
 <ul class="alternate" type="square">
-<li>GMail-syötettä ei välttämättä löydy. Tämä johtuu luultavasti siitä, että käyttäjän salasana ei täsmää käyttäjän Google-salasanaan.
-Tulevaisuudessa tämä ei ole ongelma. Toistaiseksi käyttäjät on synkronoitava Google Apps -lohkon avulla. Sähköposti
-päivitetään kirjautumisen yhteydessä. Tulevaisuudessa lukemattomat viestit päivitetään reaaliajassa.</li>
+<li>Gmail-syötettä ei välttämättä löydy. Tämä voi johtua siitä, että käyttäjän salasana ei vastaa Google-käyttäjän salasanaa.
+Jatkossa tämä ei ole ongelma. Muista toistaiseksi synkronoida käyttäjät Google Apps -lohkon kanssa. Sähköposti päivittyy
+kirjautumisen yhteydessä. Jatkossa lukemattomien viestien pitäisi päivittyä reaaliaikaisesti.</li>
 
-<li>Google-todennuslisäosan sijainti todennusjärjestyksessä on tärkeä.
-Tällä hetkellä lisäosan on korvattava käyttäjän todennustyyppi, kun Moodle-käyttäjä vaihtaa salasanaa.
-Tämä toiminta voi vaikuttaa MNet-käyttäjiin. Tähän ongelmaan ei vielä ole ratkaisua.</li>
+<li>Google-todennuksen lisäosan sijainti todennusjärjestyksessä on tärkeä.
+Tällä hetkellä sen on ohitettava käyttäjän todennustyyppi, kun Moodle-käyttäjä vaihtaa salasanan.
+Tämä toiminta voi vaikuttaa MNet-käyttäjiin. Tähän ongelmaan ei ole vielä löydetty ratkaisua.</li>
 
 </ul>
 
 </div>';
 $string['diagnostics'] = 'Diagnostiikka';
 $string['diagnostics_help'] = '<h2>Google-integroinnin diagnostiikka</h2>
-<p>Diagnostiikkasivulla on tietoa Gmail-, GData- ja SAML-yhteyden tilasta.
-Vain järjestelmänvalvojilla on oikeus tarkastella diagnostiikkatietoja.</p>
+<p>Diagnostiikkasivulla on tietoja Gmail-, GData- ja SAML-yhteyden tilasta.
+Vain järjestelmänvalvojilla on oikeus katsella diagnostiikkatietoja.</p>
 
-<h3>Sisällys:</h3>
+<h3>Sisällysluettelo:</h3>
 <ul>
-<li><a href="#setup">Määritystaulukoiden tiedot</a></li>
+<li><a href="#setup">Määritä taulukoiden tiedot</a></li>
 <li><a href="#gdata">GData-yhteyden tila</a></li>
 <li><a href="#gmail">Gmail-lohkon yhteystesti</a></li>
-<li><a href="#saml">SAML SSO -tilan testaus</a></li>
+<li><a href="#saml">SAML:n SSO-tilan testi</a></li>
 </ul>
-<h3><a name="setup" href="#setup">Määritystaulukkojen tiedot</a></h3>
+
+<h3><a name="setup" href="#setup">Määritystaulukoiden tiedot</a></h3>
 <div class="indent">
-Näissä taulukoissa on nykyiset Moodlen Google-integroinnin tiedot. Varmista, että kaikki arvot on määritetty oikein.
+Näissä taulukoissa on Moodlen ja Googlen integroinnin nykyinen määritys. Varmista, että kaikki arvot on määritetty.
 </div>
 
 <h3><a name="gdata" href="#gdata">GData-yhteys</a></h3>
 <div class="indent">
-GData-lohko, joka sijaitsee blocks-kansiossa, sisältää eri Google-palvelujen kirjastot. Varmista, että lohko pystyy muodostamaan yhteyden Google-palveluihin. Tarvittaessa voit muuttaa arvoja GData-lohkon Asetukset-sivulla.
+GData-lohko, joka on lohkojen kansiossa, sisältää eri Google-palvelujen kirjastot. Sinun on varmistettava,
+että se saa yhteyden Google-palveluihin. Sinun voi olla tarpeen säätää arvoja GData-lohkon asetussivulla.
 </div>
 
 <h3><a name="gmail" href="#gmail">Gmail-lohkon yhteystesti</a></h3>
 <div class="indent">
-Tällä hetkellä Gmail-lohko päivittää käyttäjän lukemattomat viestit ainoastaan kirjautumisen yhteydessä. Tämän testin voi suorittaa vain, kun virheenkorjaustila on käytössä. Lukemattomien viestien reaaliaikaisen tarkastelun tuki on kehityksessä.
-
+Tällä hetkellä Gmail-lohko päivittää käyttäjien lukemattomat viestit vain kirjautumisen yhteydessä. Voit
+suorittaa testin tässä vain, jos virheenkorjaus on otettu käyttöön. Tuki lukemattomien viestien reaaliaikaiselle katselemiselle on tulossa.
 </div>
 
-<h3><a name="saml" href="#saml">SAML SSO -tilan testaus</a></h3>
+<h3><a name="saml" href="#saml">SAML:n SSO-tilan testi</a></h3>
 <div class="indent">
-SAML-tilan testaus on SSO-valtuutusprosessista erillään toimiva tarkistus. Sitä ei ole vielä otettu käyttöön.
-<!--Virhetiedot saa näkyviin ottamalla kirjautumisen yhteydessä käyttöön DEBUG_DEVELOPER-virheenkorjaustilan. -->
+SAML:n tilan testi on SSO-todennusprosessista erillinen testi. Sitä ei ole vielä toteutettu.
+<!--But with debugging turned on and set to DEBUG_DEVELOPER error information should be revealed upon login -->
 </div>';
 $string['keys'] = 'Avaimet';
 $string['keys_help'] = '<span style="font-size: 1.2em">
 <h3>Googlen SSO-avaimet</h3>
-<p>Sekä Moodlen että Googlen on voitava valtuuttaa tärkeiden resurssien suojattu käyttö.
-Järjestelmänvalvojan on luotava <b>tarkistusvarmenne</b> ja ladattava julkisen avaimen sisältävä X.509-muotoinen varmenne Google-palveluun.
-<a href="http://code.google.com/apis/apps/articles/sso-keygen.html">Googlen avainten luontiin liittyvät ohjeet</a>
-Huomaa, että SAML Moodle SSO -palvelu käyttää vain <b>RSA-avaimia.</b>
-
+<p>Sekä Moodlen että Googlen on voitava varmentaa suojatusti pääsy tärkeisiin resursseihin.
+Järjestelmänvalvojan on luotava <b>Tarkistusvarmenne</b> ja ladattava Googleen X.509-muotoa oleva varmenne, jossa on upotettu
+julkinen avain.
+<a href="http://code.google.com/apis/apps/articles/sso-keygen.html">Googlen ohjeet avainten luontiin</a>
+Huomaa, että Moodlen SAML:ia käyttävä SSO-palvelu käyttää vain <b>RSA-avaimia</b>.
 </p>
 </span>
 
 <!--
-<h4><a name="pem" href="#status">Google Apps -tila</a></h4>
+<h4><a name="pem" href="#status">Google Apps Status</a></h4>
 <div class="indent">
 
 </div>
 
-<h4><a name="crt" href="#status">Varmenne</a></h4>
+<h4><a name="crt" href="#status">Certificate</a></h4>
 <div class="indent">
 -->
 </div>';
 $string['mgadget'] = 'Moodle-pienoisohjelma';
-$string['mgadget_help'] = '<h2>Moodlen Google-pienoisohjelma</h2>
+$string['mgadget_help'] = '<h2>Googlen Moodle-pienoisohjelma</h2>
 
-<p>Tämä ohjetiedosto sisältää lyhyen yleiskuvauksen Moodle-sivustoon asennettavasta Google-pienoisohjelmasta.</p>
+<p>Tässä ohjetiedostossa on lyhyt yhteenveto Moodle-sivuston Google-pienoisohjelmasta.</p>
 
 <h3><a name="status" href="#status">Moodle-pienoisohjelma</a></h3>
 <div class="indent">
-<em>Moodle-pienoisohjelma</em> on Google-kumppanisivuston aloitussivulle upotettava ohjelma.
-
+<em>Moodle-pienoisohjelma</em> on pienoisohjelma, jonka voi upottaa Google-kumppaneiden etusivulle.
 </div>
 
-<p><b>Asentaminen käyttäjän aloitussivulle</b></p>
+
+<p><b>Käyttäjien aloitussivulle asentaminen</b></p>
 <ol>
 <li>Kirjaudu Moodleen.</li>
-<li>Napsauta <b>Google-kumppanisivu</b>-linkkiä <b>Google-palvelujen käyttö</b> -lohkossa.</li>
-<li>Napsauta <b>Add Stuff</b> (Lisää sisältöä) -linkkiä.</li>
-<li>Valitse <b>Add my url</b> (Lisää URL-osoite).</li>
-<li>Anna moodlegadget-pienoisohjelman URL-osoite.</li>
-<li>URL-osoite on tämänkaltainen: <br/><b>http://www.omamoodletoimialue.org/auth/gsaml/moodlegadget.php </b>
-<li> Voit tarkistaa osoitteen hallintalohkon <b>Google-todennus</b>-lisäosan asetuksista.</li>
-<li>Kun olet kopioinut osoitteen osoitekenttään, valitse <b>Lisää</b>.</li>
-<li>Napsauta <b>Takaisin kotisivulle</b>.</li>
-<li>Näytössä pitäisi nyt näkyä lohko, jossa on linkki Moodle-sivustoon.</li>
+<li>Valitse <b>Google-kumppanisivu</b> <b>Google-palvelujen käyttö</b> -lohkossa.</li>
+<li>Napsauta <b>Lisää kohteita</b> -linkkiä.</li>
+<li>Valitse <b>Lisää oma URL</b>.</li>
+<li>Lisää moodlegadget-URL.</li>
+<li>URL-osoite voi olla esimerkiksi seuraavanlainen:<br/><b>http://www.yourmoodedomain.org/auth/gsaml/moodlegadget.php</b>
+<li>Se löytyy Hallinta-lohkon <b>Google-todennus</b>-kohdan Asetuksista.</li>
+<li>Kun olet kopioinut URL-osoitteen kenttäänsä, valitse <b>Lisää</b>.</li>
+<li>Valitse <b>Takaisin etusivulle</b>.</li>
+<li>Sinun pitäisi nähdä nyt lohko, jossa on linkki Moodle-sivustoosi.</li>
 </ol>
 
 
 
 <h3><a name="status" href="#status">Huomioitavaa (BEETA-julkaisussa)</a></h3>
 <div class="indent">
-Tämä on Moodlen Google-pienoisohjelman beetaversio. Sen hyödyt voivat moninkertaistua tulevaisuudessa.
-Pienoisohjelma ei välttämättä päivity heti asentamisen jälkeen. Tämä on tunnettu ongelma.
-Kehittäjät selvittävät tätä ongelmaa.
+Tämä on Googlen Moodle-pienoisohjelman beeta-prototyyppi. Jatkossa siitä voi olla enemmän hyötyä.
+Tällä hetkellä siinä on tunnettu virhe, joka liittyy siihen, että pienoisohjelma ei lataudu välittömästi asennuksen yhteydessä.
+Kehittäjät tutkivat asiaa.
 </div>';
 $string['event_user_authenticated'] = 'Käyttäjä todennettu';
