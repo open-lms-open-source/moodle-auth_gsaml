0b0dfcb70feb42ef85fb4c2ae571e7945275ace4
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2023-12-18 11:56:46 -0500

    INT-19330: 4.1 MP3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version      = 2023100400;
+$plugin->version      = 2023121800;
 $plugin->requires     = 2022112800;
 $plugin->component    = 'auth_gsaml';
 $plugin->release      = '4.1.3';
-$plugin->dependencies = array('local_mr' => 2023100400);
+$plugin->dependencies = array('local_mr' => 2023121800);
