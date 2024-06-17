3bf003bda45049387dc86f7e78a5c7085a7ffdf5
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-06-17 16:37:58 -0500

    INT-19886: 4.3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version      = 2024040200;
-$plugin->requires     = 2022112800;
+$plugin->version      = 2024061700;
+$plugin->requires     = 2023100900;
 $plugin->component    = 'auth_gsaml';
-$plugin->release      = '4.1.3';
-$plugin->dependencies = array('local_mr' => 2024040200);
+$plugin->release      = '4.3.4';
+$plugin->dependencies = array('local_mr' => 2024061700);
